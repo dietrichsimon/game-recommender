6bf6abab268e65bd365904b4252817be5afe5e71
[dietrichsimon] <simon.dietrich@gmail.com>
2020-08-27 16:49:51 +0200

    clean up code

--- recommender/test_application.py
+++ recommender/test_application.py
@@ -4,10 +4,6 @@ from app.py import app
 import pytest
 import requests
 
-# @pytest.fixture
-# def client():
-#     with app.test
-
 def test_response_status():
     response = requests.get('http://127.0.0.1:5000/')
     assert response.status_code == 200
